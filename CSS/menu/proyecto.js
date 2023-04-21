89e41e8b9fd0feea90f51139ebe4d1a02c582083
yulianatabo <106103026+yulianatabo@users.noreply.github.com>
2023-04-21 10:37:48 -0600

    Add files via upload

---
+++ CSS/menu/proyecto.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menuToggle = document.querySelector('.menuToggle');
+    const navegation = document.querySelector('.navegation');
+    menuToggle.classList.toggle('active');
+    navegation.classList.toggle('active');
+}
